0824f6113e5abe76e26411c35e12c2b5441c501b
Gokul <gokulyc@gmail.com>
2022-07-10 09:43:25 +0530

    modified:   docker-compose.yml modified:   producer.py

--- producer.py
+++ producer.py
@@ -6,4 +6,7 @@ topicName = 'my-topic-three'
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
 
 producer.send(topicName, b'Hello World!')
+for i in range(15):
+    t_str = f"Hello : {i}"
+    producer.send(topicName, t_str.encode('utf'))
 producer.flush()
